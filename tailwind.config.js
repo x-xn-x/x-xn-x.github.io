6af869aa049d9d07682d43272b88e2ce76a3b8a7
nm-james <68627850+x-xn-x@users.noreply.github.com>
2024-12-25 04:54:42 +1100

    v0.1.1 hotfix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,6 @@ module.exports = {
         './*.html',
     ],
     theme: {
-      screens: {
-        '3xs': '0px',
-        '2xs': '160px',
-        'xs': '320px',
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-      },
       extend: {
         colors: {
             'lime': '#00FF00',
